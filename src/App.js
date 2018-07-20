ae123e018f81e70a22910d266c1b8e58de7d563c
DandanXO <pigpig850130@gmail.com>
2018-07-20 17:03:42 +0800

    set up the project.

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {Layout, Header , Navigation, Drawer, Content } from 'react-mdl';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     /* Uses a header that scrolls with the text, rather than staying locked at the top */
+      <div className="demo-big-content">
+          <Layout>
+              <Header title="Title" scroll>
+                  <Navigation>
+                      <a href="/">Link</a>
+                      <a href="/">Link</a>
+                      <a href="/">Link</a>
+                      <a href="/">Link</a>
+                  </Navigation>
+              </Header>
+              <Drawer title="Title">
+                  <Navigation>
+                      <a href="/">Link</a>
+                      <a href="/">Link</a>
+                      <a href="/">Link</a>
+                      <a href="/">Link</a>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content" />
+              </Content>
+          </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
