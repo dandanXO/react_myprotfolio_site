3b30ce3cc48e934a95ac3a006b0bade8bca72f38
DandanXO <pigpig850130@gmail.com>
2018-07-20 19:12:08 +0800

    update link title

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Layout, Header , Navigation, Drawer, Content } from 'react-mdl';
 import './App.css';
 import Main from './components/main';
+import {Link} from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -11,10 +12,10 @@ class App extends Component {
           <Layout>
               <Header title="Title" scroll>
                   <Navigation>
-                      <a href="/">Link</a>
-                      <a href="/">Link</a>
-                      <a href="/">Link</a>
-                      <a href="/">Link</a>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/about">About</Link>
+                      <Link to="/project">Projects</Link>
+                      <Link to="/contact">Contact</Link>
                   </Navigation>
               </Header>
               <Drawer title="Title">
